1f1d0ced36b3d3f0f3c78642b67b23e465e391c9
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2020-05-17 15:05:04 +0530

    Create entropy_utils.py

---
+++ entropy_utils.py
@@ -0,0 +1,216 @@
+# All functions takes from https://github.com/raphaelvallat/entropy
+
+def petrosian_fd(x):
+    n = len(x)
+    diff = np.ediff1d(x)
+    N_delta = (diff[1:-1] * diff[0:-2] < 0).sum()
+    return np.log10(n) / (np.log10(n) + np.log10(n / (n + 0.4 * N_delta)))
+
+def katz_fd(x):
+    x = np.array(x)
+    dists = np.abs(np.ediff1d(x))
+
+    ll = dists.sum()
+    ln = np.log10(np.divide(ll, dists.mean()))
+
+    aux_d = x - x[0]
+    d = np.max(np.abs(aux_d[1:]))
+    return np.divide(ln, np.add(ln, np.log10(np.divide(d, ll))))
+
+@jit('UniTuple(float64, 2)(float64[:], float64[:])', nopython=True)
+def _linear_regression(x, y):
+    n_times = x.size
+    sx2 = 0
+    sx = 0
+    sy = 0
+    sxy = 0
+    for j in range(n_times):
+        sx2 += x[j] ** 2
+        sx += x[j]
+        sxy += x[j] * y[j]
+        sy += y[j]
+    den = n_times * sx2 - (sx ** 2)
+    num = n_times * sxy - sx * sy
+    slope = num / den
+    intercept = np.mean(y) - slope * np.mean(x)
+    return slope, intercept
+
+@jit('i8[:](f8, f8, f8)', nopython=True)
+def _log_n(min_n, max_n, factor):
+    max_i = int(floor(log(1.0 * max_n / min_n) / log(factor)))
+    ns = [min_n]
+    for i in range(max_i + 1):
+        n = int(floor(min_n * (factor ** i)))
+        if n > ns[-1]:
+            ns.append(n)
+    return np.array(ns, dtype=np.int64)
+
+@jit('float64(float64[:], int32)')
+def _higuchi_fd(x, kmax):
+    n_times = x.size
+    lk = np.empty(kmax)
+    x_reg = np.empty(kmax)
+    y_reg = np.empty(kmax)
+    for k in range(1, kmax + 1):
+        lm = np.empty((k,))
+        for m in range(k):
+            ll = 0
+            n_max = floor((n_times - m - 1) / k)
+            n_max = int(n_max)
+            for j in range(1, n_max):
+                ll += abs(x[m + j * k] - x[m + (j - 1) * k])
+            ll /= k
+            ll *= (n_times - 1) / (k * n_max)
+            lm[m] = ll
+        # Mean of lm
+        m_lm = 0
+        for m in range(k):
+            m_lm += lm[m]
+        m_lm /= k
+        lk[k - 1] = m_lm
+        x_reg[k - 1] = log(1. / k)
+        y_reg[k - 1] = log(m_lm)
+    higuchi, _ = _linear_regression(x_reg, y_reg)
+    return higuchi
+
+def higuchi_fd(x, kmax=10):
+    x = np.asarray(x, dtype=np.float64)
+    kmax = int(kmax)
+    return _higuchi_fd(x, kmax)
+    
+def _embed(x, order=3, delay=1):
+    N = len(x)
+    if order * delay > N:
+        raise ValueError("Error: order * delay should be lower than x.size")
+    if delay < 1:
+        raise ValueError("Delay has to be at least 1.")
+    if order < 2:
+        raise ValueError("Order has to be at least 2.")
+    Y = np.zeros((order, N - (order - 1) * delay))
+    for i in range(order):
+        Y[i] = x[i * delay:i * delay + Y.shape[1]]
+    return Y.T
+
+def perm_entropy(x, order=3, delay=1, normalize=False):
+    x = np.array(x)
+    ran_order = range(order)
+    hashmult = np.power(order, ran_order)
+    sorted_idx = _embed(x, order=order, delay=delay).argsort(kind='quicksort')
+    hashval = (np.multiply(sorted_idx, hashmult)).sum(1)
+    _, c = np.unique(hashval, return_counts=True)
+    p = np.true_divide(c, c.sum())
+    pe = -np.multiply(p, np.log2(p)).sum()
+    if normalize:
+        pe /= np.log2(factorial(order))
+    return pe
+
+def spectral_entropy(x, sf, method='fft', nperseg=None, normalize=False):
+    x = np.array(x)
+    if method == 'fft':
+        _, psd = periodogram(x, sf)
+    elif method == 'welch':
+        _, psd = welch(x, sf, nperseg=nperseg)
+    psd_norm = np.divide(psd, psd.sum())
+    se = -np.multiply(psd_norm, np.log2(psd_norm)).sum()
+    if normalize:
+        se /= np.log2(psd_norm.size)
+    return se
+
+def svd_entropy(x, order=3, delay=1, normalize=False):
+    x = np.array(x)
+    mat = _embed(x, order=order, delay=delay)
+    W = np.linalg.svd(mat, compute_uv=False)
+    W /= sum(W)
+    svd_e = -np.multiply(W, np.log2(W)).sum()
+    if normalize:
+        svd_e /= np.log2(order)
+    return svd_e
+
+def _app_samp_entropy(x, order, metric='chebyshev', approximate=True):
+    _all_metrics = KDTree.valid_metrics
+    if metric not in _all_metrics:
+        raise ValueError('The given metric (%s) is not valid. The valid '
+                         'metric names are: %s' % (metric, _all_metrics))
+    phi = np.zeros(2)
+    r = 0.2 * np.std(x, axis=-1, ddof=1)
+
+    _emb_data1 = _embed(x, order, 1)
+    if approximate:
+        emb_data1 = _emb_data1
+    else:
+        emb_data1 = _emb_data1[:-1]
+    count1 = KDTree(emb_data1, metric=metric).query_radius(emb_data1, r,
+                                                           count_only=True
+                                                           ).astype(np.float64)
+    emb_data2 = _embed(x, order + 1, 1)
+    count2 = KDTree(emb_data2, metric=metric).query_radius(emb_data2, r,
+                                                           count_only=True
+                                                           ).astype(np.float64)
+    if approximate:
+        phi[0] = np.mean(np.log(count1 / emb_data1.shape[0]))
+        phi[1] = np.mean(np.log(count2 / emb_data2.shape[0]))
+    else:
+        phi[0] = np.mean((count1 - 1) / (emb_data1.shape[0] - 1))
+        phi[1] = np.mean((count2 - 1) / (emb_data2.shape[0] - 1))
+    return phi
+
+@jit('f8(f8[:], i4, f8)', nopython=True)
+def _numba_sampen(x, mm=2, r=0.2):
+    n = x.size
+    n1 = n - 1
+    mm += 1
+    mm_dbld = 2 * mm
+    r *= x.std()
+
+    run = [0] * n
+    run1 = run[:]
+    r1 = [0] * (n * mm_dbld)
+    a = [0] * mm
+    b = a[:]
+    p = a[:]
+
+    for i in range(n1):
+        nj = n1 - i
+
+        for jj in range(nj):
+            j = jj + i + 1
+            if abs(x[j] - x[i]) < r:
+                run[jj] = run1[jj] + 1
+                m1 = mm if mm < run[jj] else run[jj]
+                for m in range(m1):
+                    a[m] += 1
+                    if j < n1:
+                        b[m] += 1
+            else:
+                run[jj] = 0
+        for j in range(mm_dbld):
+            run1[j] = run[j]
+            r1[i + n * j] = run[j]
+        if nj > mm_dbld - 1:
+            for j in range(mm_dbld, nj):
+                run1[j] = run[j]
+
+    m = mm - 1
+
+    while m > 0:
+        b[m] = b[m - 1]
+        m -= 1
+
+    b[0] = n * n1 / 2
+    a = np.array([float(aa) for aa in a])
+    b = np.array([float(bb) for bb in b])
+    p = np.true_divide(a, b)
+    return -log(p[-1])
+
+def app_entropy(x, order=2, metric='chebyshev'):
+    phi = _app_samp_entropy(x, order=order, metric=metric, approximate=True)
+    return np.subtract(phi[0], phi[1])
+
+def sample_entropy(x, order=2, metric='chebyshev'):
+    x = np.asarray(x, dtype=np.float64)
+    if metric == 'chebyshev' and x.size < 5000:
+        return _numba_sampen(x, mm=order, r=0.2)
+    else:
+        phi = _app_samp_entropy(x, order=order, metric=metric,
+                                approximate=False)
+        return -np.log(np.divide(phi[1], phi[0]))
